35025e6fb603bd8e4117d58d1474cbe769d1ac18
Emmanuel Egito Oliveira Candeira Marinho <e_egito@yahoo.com>
2025-02-03 23:32:27 -0300

    Montagem inicial do backend e do frontend

---
+++ Calculo.Cdb.App/backend/Calculo.Cdb.App.Api/Controllers/CalculationsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Calculo.Cdb.App.Api.Controllers
+{
+    [Route("api/calculations")]
+    public class CalculationsController : ApiController
+    {
+        // GET: api/Calculations
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Calculations/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Calculations
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Calculations/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Calculations/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Calculo.Cdb.App/backend/Calculo.Cdb.App.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Calculo.Cdb.App.Api.WebApiApplication" Language="C#" %>
