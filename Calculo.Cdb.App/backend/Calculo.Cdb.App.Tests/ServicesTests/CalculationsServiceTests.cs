44ed486ce0a2e9810b07e35ed7288174658aa390
Emmanuel Marinho <t_emmanuel.marinho@hapvida.com.br>
2025-02-04 18:33:34 -0300

    criando testes para o servico de calculo do cdb

---
+++ Calculo.Cdb.App/backend/Calculo.Cdb.App.Tests/ServicesTests/CalculationsServiceTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculo.Cdb.App.Tests.ServicesTests;
+
+public class CalculationsServiceTests
+{
+    [Fact]
+    public void DeveRetornarErroCasoMesSejaMenorIgualZero
+    {
+        // Arrange
+        var mockRepositorio = new Mock<IRepositorioCliente>();
+        var servico = new ServicoCliente(mockRepositorio.Object);
+
+    // Act & Assert
+    Assert.Throws<ArgumentException>(() => servico.ObterClientePorId(0));
+    }
+}
